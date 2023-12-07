fc2cdd1d807c986576dc35831f29fadad6a3dc3c
Eduardo Rodriguez <eduardorodriguez@eduardos-macbook-pro.home>
2023-12-07 22:14:03 +0100

    [chore] all replication

---
+++ TabbedPAge/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Maui.Controls.PlatformConfiguration.AndroidSpecific;
+using Application = Microsoft.Maui.Controls.Application;
+using TabbedPage = Microsoft.Maui.Controls.TabbedPage;
+
+namespace TabbedPAge;
+
+public partial class App : Application
+{
+    public App()
+    {
+        InitializeComponent();
+        
+        var tabbedPage = new TabbedPage
+            {
+                Children =
+                {
+                    new ContentPage() { Title = "Page1"}
+                    ,new ContentPage() { Title = "Page2"}
+                    ,new ContentPage() { Title = "Page3"}
+                
+                }
+            };
+
+        tabbedPage.On<Microsoft.Maui.Controls.PlatformConfiguration.Android>()
+            .SetToolbarPlacement(ToolbarPlacement.Bottom);
+
+        MainPage = tabbedPage;
+    }
+}
